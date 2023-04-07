115b12c906578a62167a8689ffb02642ee3b2793
APURV <apoorv.agarwal@velotio.com>
2023-04-07 11:55:35 +0530

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    path: "banner",
+    label: "Home",
+  },
+  {
+    path: "boost-agencies",
+    label: "Services",
+  },
+  {
+    path: "services",
+    label: "Our Data",
+  },
+];

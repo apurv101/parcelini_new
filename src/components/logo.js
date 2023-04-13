a676991bdfbfab17e256e845a8c22d995dbe97b0
APURV <apoorvagarwal00@gmail.com>
2023-04-13 11:22:46 +0530

    logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,22 @@
 /** @jsx jsx */
-import { jsx, Image } from 'theme-ui';
-import { Link } from 'components/link';
-import logo from 'assets/logo.svg';
+import { jsx, Image } from "theme-ui";
+import { Link } from "components/link";
+// import logo from 'assets/logo.svg';
+import logo from "assets/bitmap-2-2.png";
 
 export default function Logo() {
   return (
     <Link
       path="/"
       sx={{
-        variant: 'links.logo',
+        variant: "links.logo",
       }}
     >
-      <Image src={logo} sx={{ display: 'flex' }} alt="startup landing logo" />
+      <Image
+        src={logo}
+        sx={{ display: "flex", width: "40%", height: "auto" }}
+        alt="parcelini logo"
+      />
     </Link>
   );
 }
